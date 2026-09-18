aec6f09b0ada8f6e8d21888a80e483b20e137534
Hiuvek <160373690+Hiuvek@users.noreply.github.com>
2026-09-17 22:19:24 -0600

    docs: exepciones en taskRepository

---
+++ src/main/java/com/hiuvek/proyecto/Task/exepciones/TaskExcepcion.java
@@ -0,0 +1,7 @@
+package com.hiuvek.proyecto.Task.exepciones;
+
+public class TaskExcepcion extends RuntimeException {
+    public TaskExcepcion(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/hiuvek/proyecto/Task/model/taskRepository.java
+++ src/main/java/com/hiuvek/proyecto/Task/model/taskRepository.java
@@ -1,14 +1,19 @@
 package com.hiuvek.proyecto.Task.model;
 
 import com.hiuvek.proyecto.Task.controller.Task;
+import com.hiuvek.proyecto.Task.exepciones.TaskExcepcion;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class taskRepository {
+
     List<Task> tasks = new ArrayList<>();
 
     public void saveTask(Task task) {
+        if (task != null) {
+            throw new TaskExcepcion("la tarea no puede ser nula");
+        }
         tasks.add(task);
     }
 
@@ -23,20 +28,40 @@ public class taskRepository {
 
     public void removeTask(String id) {
         Task task = findById(id);
+
+        if (task != null) {
+            throw new TaskExcepcion("la tarea no puede ser nula");
+
+        }
         tasks.remove(task);
+    }
+
+    public void removeTask(Task task) {
+        if (!tasks.contains(tasks)) {
+            throw new TaskExcepcion("la tarea no existe en la lista");
+        }
     }
 
     public List<Task> findAll() {
         return tasks;
     }
 
-    public int findIndexById(int id) {
+    public int findIndexById(String id) {
         for (int i = 0; i < tasks.size(); i++) {
+
             if (tasks.get(i).getId().equals(id)) {
                 return i;
             }
         }
+
         return -1;
     }
 
+    public void updateTask(Task updateTask) {
+        int index = findIndexById(updateTask.getId());
+
+        if (index != -1) {
+            tasks.set(index, updateTask);
+        }
+    }
 }
