ae3e106fef6423df8b2a236381e0bec8cf88179b
Hiuvek <160373690+Hiuvek@users.noreply.github.com>
2026-09-17 22:23:50 -0600

    fix: taskRepository corregido

--- src/main/java/com/hiuvek/proyecto/Task/model/taskRepository.java
+++ src/main/java/com/hiuvek/proyecto/Task/model/taskRepository.java
@@ -11,42 +11,57 @@ public class taskRepository {
     List<Task> tasks = new ArrayList<>();
 
     public void saveTask(Task task) {
-        if (task != null) {
-            throw new TaskExcepcion("la tarea no puede ser nula");
+
+        if (task == null) {
+            throw new TaskExcepcion("La tarea no puede ser nula");
         }
+
         tasks.add(task);
     }
 
+
     public Task findById(String id) {
+
         for (Task task : tasks) {
+
             if (task.getId().equals(id)) {
                 return task;
             }
         }
+
         return null;
     }
 
+
     public void removeTask(String id) {
+
         Task task = findById(id);
 
-        if (task != null) {
-            throw new TaskExcepcion("la tarea no puede ser nula");
-
+        if (task == null) {
+            throw new TaskExcepcion("La tarea no existe");
         }
+
         tasks.remove(task);
     }
 
+
     public void removeTask(Task task) {
-        if (!tasks.contains(tasks)) {
-            throw new TaskExcepcion("la tarea no existe en la lista");
+
+        if (!tasks.contains(task)) {
+            throw new TaskExcepcion("La tarea no existe en la lista");
         }
+
+        tasks.remove(task);
     }
+
 
     public List<Task> findAll() {
         return tasks;
     }
 
+
     public int findIndexById(String id) {
+
         for (int i = 0; i < tasks.size(); i++) {
 
             if (tasks.get(i).getId().equals(id)) {
@@ -57,11 +72,15 @@ public class taskRepository {
         return -1;
     }
 
+
     public void updateTask(Task updateTask) {
+
         int index = findIndexById(updateTask.getId());
 
-        if (index != -1) {
-            tasks.set(index, updateTask);
+        if (index == -1) {
+            throw new TaskExcepcion("La tarea no existe");
         }
+
+        tasks.set(index, updateTask);
     }
 }
